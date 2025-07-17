9455cc8e0204a89880e6660c1dc7070c0584b5ac
omkaaaar <75777792+omkaaaar@users.noreply.github.com>
2025-07-17 22:34:31 +0530

    feat: update route to handle dynamic parameter for greeting

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/get/:id", (req, res) => {
+  console.log(req.params);
+
   res.send("Hello World!");
 });
 
