2d754144008a7368c3d80c6a51ac6280ad93cce8
omkaaaar <75777792+omkaaaar@users.noreply.github.com>
2025-07-20 20:35:39 +0530

    feat: refactor user signup endpoint to accept dynamic user data and add CRUD operations for users

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// const { ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -5,20 +6,48 @@ const port = 3000;
 const connectDB = require("./config/db");
 const User = require("../backend/models/user");
 
+app.use(express.json());
+
 // ! Posting a user to DB
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    name: "Omkar",
-    lastName: "Kallekar",
-    email: "omkar.kallekar@gmail.com",
-    age: 24,
-    gender: "male",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added succesfully");
   } catch (error) {
+    res.status(400).send("Bad request");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (err) {
+    res.status(400).send("Bad request");
+  }
+});
+
+app.delete("/deluser", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    // console.log(deleteUser);
+
+    res.send("User deleted succesfully");
+  } catch (err) {
+    res.status(400).send("Bad request");
+  }
+});
+
+app.put("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const user = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, user);
+    res.send("User updated succesfully");
+  } catch (err) {
     res.status(400).send("Bad request");
   }
 });
