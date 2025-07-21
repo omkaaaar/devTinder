810625fcf5b0f7513cf6974b4b2e756a3ac814a2
omkaaaar <75777792+omkaaaar@users.noreply.github.com>
2025-07-21 23:44:31 +0530

    feat: enhance user update functionality with validation and dynamic userId parameter

--- backend/index.js
+++ backend/index.js
@@ -44,14 +44,35 @@ app.delete("/deluser", async (req, res) => {
 });
 
 // ! Updating
-app.put("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.put("/user/:userId", async (req, res) => {
+  const userId = req.params.userId;
   const user = req.body;
   try {
-    await User.findByIdAndUpdate({ _id: userId }, user);
+    const allowedUpdate = [
+      "firstName",
+      "lastName",
+      "password",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+
+    const isUpdateAllowed = Object.keys(user).every((k) =>
+      allowedUpdate.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Cannot update");
+    }
+    if (user?.skills.length > 10) {
+      throw new Error("skills cannot exceed the limit of 10");
+    }
+    await User.findByIdAndUpdate({ _id: userId }, user, {
+      runValidators: true,
+    });
     res.send("User updated succesfully");
   } catch (err) {
-    res.status(400).send("Bad request");
+    res.status(400).send("Bad request " + err.message);
   }
 });
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,33 +1,57 @@
 const mongoose = require("mongoose");
+var validator = require("validator");
+const { default: isEmail } = require("validator/lib/isEmail");
 
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Invalid gender type");
+        }
+      },
+    },
+    skills: {
+      type: [],
+    },
+    about: {
+      type: String,
+      default: "This is about me",
+      max: 300,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-  about: {
-    type: String,
-    default: "This is about me",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
